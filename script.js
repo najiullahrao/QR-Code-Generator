6dcf736e350c8711a33c100d16d8944c1ea46d56
najiullahrao <142686847+najiullahrao@users.noreply.github.com>
2025-02-13 23:30:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function generateQR() {
         qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
         qrImg.style.display = "block";
     } else {
+        qrImg.style.display = "none";
         errorSound.play();
         qrText.classList.add('error');
         setTimeout(() => {
